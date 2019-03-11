5a96398070de2a080fab52ed32d9eb8ecf5c00ed
Leonardo Zeaim <leonardozeaim@gmail.com>
2019-03-11 02:43:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var name = "Joe"
 var height = "74"
+// Don't worry about this
+module.exports = { name, height
+}
+
 
 var index = require("./index.js")
 
 if (index.name === "Joe") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Susan, Received: "+index.name)
+    console.log("Expected: Joe, Received: "+index.name)
 }
 
 
@@ -14,9 +18,6 @@ if (index.name === "Joe") {
 if (index.height === 74) {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected: 74, Received: "+index.height)
 }
 
-// Don't worry about this
-module.exports = { name, height
-}
